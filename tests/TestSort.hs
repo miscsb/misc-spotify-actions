d4d471605ffdd9f0941c82b859a0d55ba9ca74e4
Samuel Buena <sbuena@cs.stonybrook.edu>
2024-01-22 19:05:27 -0500

    add tests

--- tests/TestSort.hs
+++ tests/TestSort.hs
@@ -7,10 +7,34 @@ import Data.Monoid
 import Control.Monad
 import Misc.Util
 
-sortTest :: Assertion
-sortTest = print "Sort Test"
+interactiveSort :: [Int] -> [Int]
+interactiveSort xs =
+    let mergeSort = initialMergeSortState xs
+        comparator (Just (valueLeft, valueRight)) = compare valueLeft valueRight
+        comparator Nothing = error "impossible"
+        mergeSort' = until (\state -> case state of 
+            MergeSortComplete _ -> True
+            MergeSortIncomplete _ -> False) (\state -> stepMergeSort state (comparator (currentMergeSortComparison state))) mergeSort
+    in case mergeSort' of
+        MergeSortComplete sorted -> sorted
+        MergeSortIncomplete _ -> error "impossible"
+
+sortTest1 :: Assertion
+sortTest1 = assertEqual "should be sorted" [8,7..1] (interactiveSort [8,1,7,2,6,3,5,4])
+
+sortTest2 :: Assertion
+sortTest2 = assertEqual "should be sorted" [9,8..1] (interactiveSort [1,7,2,6,9,3,5,8,4])
+
+sortTest3 :: Assertion
+sortTest3 = assertEqual "should be sorted" [] (interactiveSort [])
+
+sortTest4 :: Assertion
+sortTest4 = assertEqual "should be sorted" [1] (interactiveSort [1])
 
 main :: IO ()
 main = defaultMainWithOpts
-       [testCase "sortTest" sortTest]
-       mempty
+    [testCase "sort even list" sortTest1,
+     testCase "sort odd list" sortTest2,
+     testCase "sort empty list" sortTest3,
+     testCase "sort singleton list" sortTest4]
+    mempty
